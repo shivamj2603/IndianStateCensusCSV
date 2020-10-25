7d00286e036e432bcf018c64e0f8483d2621d9e2
shivamj2603 <shivamj2603@gmail.com>
2020-10-26 01:15:23 +0530

    Welcome to Indian State Census

---
+++ src/main/java/com/indianstatecensus/CensusAnalyserException.java
@@ -0,0 +1,12 @@
+package com.indianstatecensus;
+
+public class CensusAnalyserException extends Exception {
+	public enum ExceptionType {
+		UNABLE_TO_PARSE, INCORRECT_FILE
+	}
+	public ExceptionType type;
+	public CensusAnalyserException(String message, ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+}
---
+++ src/test/java/com/test/indianstatecensus/CensusAnalyserTest.java
@@ -0,0 +1,68 @@
+package com.test.indianstatecensus;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import com.indianstatecensus.CensusAnalyser;
+import com.indianstatecensus.CensusAnalyserException;
+
+public class CensusAnalyserTest {
+	private static final String STATECENSUS_CSVFILE = "C:\\Users\\shiva\\eclipse-workspace\\Census\\IndiaStateCensusData.csv";
+	private static final String WRONG_FILE = "C:\\Users\\shiva\\eclipse-workspace\\Census\\IndianStateCensus.csv";
+	private static final String WRONG_EXTENSION = "C:\\Users\\shiva\\eclipse-workspace\\Census\\IndiaStateCensusData.txt";
+	private static final String CSVFILE = "C:\\Users\\shiva\\eclipse-workspace\\Census\\USCensusData.csv";
+	@Test
+	public void givenCSVFile_ifMatchesTotalNumberOfRecords_ShouldReturnTrue() throws IOException {
+		CensusAnalyser analyser = new CensusAnalyser();
+		int count = 0;
+		try {
+			count = analyser.loadCSVData(STATECENSUS_CSVFILE);
+		} catch (CensusAnalyserException exception) {
+			exception.printStackTrace();
+		}
+		assertEquals(29, count);
+	}
+	@Test
+	public void givenCSVFile_whenWrongFile_ShouldThrowError() throws IOException {
+		CensusAnalyser analyser = new CensusAnalyser();
+		int count = 0;
+		try {
+			count = analyser.loadCSVData(WRONG_FILE);
+		} catch (CensusAnalyserException exception) {
+			System.out.println("No such file found");
+			assertEquals(CensusAnalyserException.ExceptionType.INCORRECT_FILE, exception.type);
+		}
+	}
+	@Test
+	public void givenCSVFile_whenFileCorrect_butExtensionIncorrect_shouldThrowError() throws IOException {
+		CensusAnalyser analyser = new CensusAnalyser();
+		int count = 0;
+		try {
+			count = analyser.loadCSVData(WRONG_EXTENSION);
+		} catch (CensusAnalyserException exception) {
+			System.out.println("No such file found");
+			assertEquals(CensusAnalyserException.ExceptionType.INCORRECT_FILE, exception.type);
+		}
+	}
+	@Test
+	public void givenCSVFile_whenFileCorrect_butDelimiterIncorrect_shouldThrowError() throws IOException {
+		CensusAnalyser analyser = new CensusAnalyser();
+		int count = 0;
+		try {
+			count = analyser.loadCSVData(STATECENSUS_CSVFILE);
+		} catch (CensusAnalyserException exception) {
+			System.out.println("Incorrect file");
+			assertEquals(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, exception.type);
+		}
+	}
+	@Test
+	public void givenCSVFile_whenFileCorrect_butHeaderIncorrect_shouldThrowError() throws IOException {
+		CensusAnalyser analyser = new CensusAnalyser();
+		int count = 0;
+		try {
+			count = analyser.loadCSVData(CSVFILE);
+		} catch (CensusAnalyserException exception) {
+			System.out.println("Incorrect file");
+			assertEquals(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, exception.type);
+		}
+	}
+}
