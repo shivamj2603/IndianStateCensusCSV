2cdce8392dcde33943c817a48a249f289f7b180d
shivamj2603 <shivamj2603@gmail.com>
2020-10-26 01:21:55 +0530

    Added:CensusAnalyser to load the IndianStateCensusData

---
+++ src/main/java/com/indianstatecensus/CensusAnalyser.java
@@ -0,0 +1,32 @@
+package com.indianstatecensus;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class CensusAnalyser {
+	public int loadCSVData(String csvFile) throws CensusAnalyserException, IOException {
+		try(Reader reader = Files.newBufferedReader(Paths.get(csvFile));) {
+			CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder = new CsvToBeanBuilder<CSVStateCensus>(reader);
+			csvToBeanBuilder.withType(CSVStateCensus.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<CSVStateCensus> csvToBean = csvToBeanBuilder.build();
+			Iterator<CSVStateCensus> censusIterator = csvToBean.iterator();
+			int countOfRecord = 0;
+			while (censusIterator.hasNext()) {
+				countOfRecord++;
+				CSVStateCensus censusData = censusIterator.next();
+			}
+			return countOfRecord;
+		} 
+		catch (IOException exception) {
+			throw new CensusAnalyserException(exception.getMessage(), CensusAnalyserException.ExceptionType.INCORRECT_FILE);
+		}
+		catch (IllegalStateException exception) {
+			throw new CensusAnalyserException(exception.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+		}
+	}
+}
