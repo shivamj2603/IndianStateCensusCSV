b58faa4f20716632360ea2ddd01b3dd7b584509a
shivamj2603 <shivamj2603@gmail.com>
2020-10-26 01:24:34 +0530

    Tested:Whether the number of record matches the CSV file read

--- src/test/java/com/test/indianstatecensus/CensusAnalyserTest.java
+++ src/test/java/com/test/indianstatecensus/CensusAnalyserTest.java
@@ -7,9 +7,6 @@ import com.indianstatecensus.CensusAnalyserException;
 
 public class CensusAnalyserTest {
 	private static final String STATECENSUS_CSVFILE = "C:\\Users\\shiva\\eclipse-workspace\\Census\\IndiaStateCensusData.csv";
-	private static final String WRONG_FILE = "C:\\Users\\shiva\\eclipse-workspace\\Census\\IndianStateCensus.csv";
-	private static final String WRONG_EXTENSION = "C:\\Users\\shiva\\eclipse-workspace\\Census\\IndiaStateCensusData.txt";
-	private static final String CSVFILE = "C:\\Users\\shiva\\eclipse-workspace\\Census\\USCensusData.csv";
 	@Test
 	public void givenCSVFile_ifMatchesTotalNumberOfRecords_ShouldReturnTrue() throws IOException {
 		CensusAnalyser analyser = new CensusAnalyser();
@@ -20,49 +17,5 @@ public class CensusAnalyserTest {
 			exception.printStackTrace();
 		}
 		assertEquals(29, count);
-	}
-	@Test
-	public void givenCSVFile_whenWrongFile_ShouldThrowError() throws IOException {
-		CensusAnalyser analyser = new CensusAnalyser();
-		int count = 0;
-		try {
-			count = analyser.loadCSVData(WRONG_FILE);
-		} catch (CensusAnalyserException exception) {
-			System.out.println("No such file found");
-			assertEquals(CensusAnalyserException.ExceptionType.INCORRECT_FILE, exception.type);
-		}
-	}
-	@Test
-	public void givenCSVFile_whenFileCorrect_butExtensionIncorrect_shouldThrowError() throws IOException {
-		CensusAnalyser analyser = new CensusAnalyser();
-		int count = 0;
-		try {
-			count = analyser.loadCSVData(WRONG_EXTENSION);
-		} catch (CensusAnalyserException exception) {
-			System.out.println("No such file found");
-			assertEquals(CensusAnalyserException.ExceptionType.INCORRECT_FILE, exception.type);
-		}
-	}
-	@Test
-	public void givenCSVFile_whenFileCorrect_butDelimiterIncorrect_shouldThrowError() throws IOException {
-		CensusAnalyser analyser = new CensusAnalyser();
-		int count = 0;
-		try {
-			count = analyser.loadCSVData(STATECENSUS_CSVFILE);
-		} catch (CensusAnalyserException exception) {
-			System.out.println("Incorrect file");
-			assertEquals(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, exception.type);
-		}
-	}
-	@Test
-	public void givenCSVFile_whenFileCorrect_butHeaderIncorrect_shouldThrowError() throws IOException {
-		CensusAnalyser analyser = new CensusAnalyser();
-		int count = 0;
-		try {
-			count = analyser.loadCSVData(CSVFILE);
-		} catch (CensusAnalyserException exception) {
-			System.out.println("Incorrect file");
-			assertEquals(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, exception.type);
-		}
 	}
 }
