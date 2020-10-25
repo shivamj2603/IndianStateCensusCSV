2e588b0ce36827992ea6ff17f1a74069277ef613
shivamj2603 <shivamj2603@gmail.com>
2020-10-26 02:05:02 +0530

    Added:Functionality to read StateCodeCSV data

--- src/main/java/com/indianstatecensus/CensusAnalyser.java
+++ src/main/java/com/indianstatecensus/CensusAnalyser.java
@@ -30,4 +30,25 @@ public class CensusAnalyser {
 			throw new CensusAnalyserException(exception.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
 		}
 	}
+	public int loadStateCodeData(String csvFile) throws CensusAnalyserException, IOException {
+		try(Reader reader = Files.newBufferedReader(Paths.get(csvFile));) {
+			CsvToBeanBuilder<StateCodeCSV> csvToBeanBuilder = new CsvToBeanBuilder<StateCodeCSV>(reader);
+			csvToBeanBuilder.withType(StateCodeCSV.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<StateCodeCSV> csvToBean = csvToBeanBuilder.build();
+			Iterator<StateCodeCSV> censusIterator = csvToBean.iterator();
+			int countOfRecord = 0;
+			while (censusIterator.hasNext()) {
+				countOfRecord++;
+				StateCodeCSV censusData = censusIterator.next();
+			}
+			return countOfRecord;
+		} 
+		catch (IOException exception) {
+			throw new CensusAnalyserException(exception.getMessage(), CensusAnalyserException.ExceptionType.INCORRECT_FILE);
+		}
+		catch (RuntimeException exception) {
+			throw new CensusAnalyserException(exception.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+		}
+	}
 }
