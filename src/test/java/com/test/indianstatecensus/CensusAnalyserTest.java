85f8ef0f0e876d158c122882aeaa7997f6e66c1a
shivamj2603 <shivamj2603@gmail.com>
2020-10-26 01:34:49 +0530

    Added:Testcase to check whether delimiter correct

--- src/test/java/com/test/indianstatecensus/CensusAnalyserTest.java
+++ src/test/java/com/test/indianstatecensus/CensusAnalyserTest.java
@@ -9,6 +9,7 @@ public class CensusAnalyserTest {
 	private static final String STATECENSUS_CSVFILE = "C:\\Users\\shiva\\eclipse-workspace\\Census\\IndiaStateCensusData.csv";
 	private static final String WRONG_FILE = "C:\\Users\\shiva\\eclipse-workspace\\Census\\IndianStateCensus.csv";
 	private static final String WRONG_EXTENSION = "C:\\Users\\shiva\\eclipse-workspace\\Census\\IndiaStateCensusData.txt";
+	private static final String CSVFILE = "C:\\Users\\shiva\\eclipse-workspace\\Census\\USCensusData.csv";
 	@Test
 	public void givenCSVFile_ifMatchesTotalNumberOfRecords_ShouldReturnTrue() throws IOException {
 		CensusAnalyser analyser = new CensusAnalyser();
@@ -40,6 +41,17 @@ public class CensusAnalyserTest {
 		} catch (CensusAnalyserException exception) {
 			System.out.println("No such file found");
 			assertEquals(CensusAnalyserException.ExceptionType.INCORRECT_FILE, exception.type);
+		}
+	}
+	@Test
+	public void givenCSVFile_whenFileCorrect_butDelimiterIncorrect_shouldThrowError() throws IOException {
+		CensusAnalyser analyser = new CensusAnalyser();
+		int count = 0;
+		try {
+			count = analyser.loadCSVData(STATECENSUS_CSVFILE);
+		} catch (CensusAnalyserException exception) {
+			System.out.println("Incorrect file");
+			assertEquals(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, exception.type);
 		}
 	}
 }
