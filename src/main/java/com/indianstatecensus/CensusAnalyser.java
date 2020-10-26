dc806cba537e84fb6b0ab179986755470e75cdd8
shivamj2603 <shivamj2603@gmail.com>
2020-10-27 05:17:56 +0530

    Added:Functionality to sort the IndianStateCensus data based on State Name

--- src/main/java/com/indianstatecensus/CensusAnalyser.java
+++ src/main/java/com/indianstatecensus/CensusAnalyser.java
@@ -44,4 +44,25 @@ public class CensusAnalyser {
 			throw new CensusAnalyserException(exception.getMessage(), CensusAnalyserException.ExceptionType.INCORRECT_FILE);
 		}
 	}
+	public String getStateWiseSortedCensusData() throws CensusAnalyserException {
+		if(censusCSVList == null || censusCSVList.size() == 0) {
+			throw new CensusAnalyserException("No Census Data", CensusAnalyserException.ExceptionType.NO_CENSUS_DATA);
+		}
+		Comparator<CSVStateCensus> censusComparator = Comparator.comparing(census -> census.state);
+		this.sort(censusComparator);
+		String sortedStateCensusJson = new Gson().toJson(censusCSVList);
+		return sortedStateCensusJson;
+	}
+	private void sort(Comparator<CSVStateCensus> censusComparator) {
+		for (int i = 0; i < censusCSVList.size(); i++) {
+			for (int j = 0; j < censusCSVList.size() - i - 1; j++) {
+				CSVStateCensus census1 = censusCSVList.get(j);
+				CSVStateCensus census2 = censusCSVList.get(j + 1);
+				if (censusComparator.compare(census1, census2) > 0) {
+					censusCSVList.set(j, census2);
+					censusCSVList.set(j + 1, census1);
+				}
+			}
+		}
+	}
 }
