9b7b488c2b6d82201cdba9a7621da9b151a69a36
shivamj2603 <shivamj2603@gmail.com>
2020-10-27 07:08:18 +0530

    Added:Functionality to sort by area

--- src/main/java/com/indianstatecensus/CensusAnalyser.java
+++ src/main/java/com/indianstatecensus/CensusAnalyser.java
@@ -106,5 +106,14 @@ public class CensusAnalyser {
 		this.sort(censusCSVList, censusComparator.reversed());
 		String sortedStateCensusJson = new Gson().toJson(censusCSVList);
 		return sortedStateCensusJson;
+	}	
+	public String getAreaWiseSortedCensusData() throws CensusAnalyserException {
+		if(censusCSVList == null || censusCSVList.size() == 0) {
+			throw new CensusAnalyserException("No Census Data", CensusAnalyserException.ExceptionType.NO_CENSUS_DATA);
+		}
+		Comparator<CSVStateCensus> censusComparator = Comparator.comparing(census -> census.areaInSqKm);
+		this.sort(censusCSVList, censusComparator.reversed());
+		String sortedStateCensusJson = new Gson().toJson(censusCSVList);
+		return sortedStateCensusJson;
 	}		
 }
